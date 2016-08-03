a909b1be097c91af3cd6321ea282c0e29a9144e1
84Engineer <alx84@ukr.net>
2016-08-03 18:44:51 +0300

    queue added but not finished

--- Algorithms/src/org/lafore/datastructures/Queue.java
+++ Algorithms/src/org/lafore/datastructures/Queue.java
@@ -9,6 +9,38 @@ package org.lafore.datastructures;
  *
  * @author oslysenko
  */
-public class Queue {
+public class Queue<T> {
+    
+    private Object[] elements;
+    private int front = 0;
+    private int rear = -1;
+    private int items = 0;
+    
+    public Queue(int size) {
+        elements = new Object[size];
+    }
+    
+    public void insert(T element) {
+        if(items < elements.length) {
+            if(rear == elements.length - 1) {
+                rear = - 1;
+            }
+            elements[++rear] = element;
+            items++;
+        }
+    }
+    
+    public T remove() {
+        if(items > 0) {
+            T temp = (T) elements[front++];
+            if(front == elements.length) {
+                front = 0;
+            }
+            items--;
+            return temp;
+        }
+        return null;
+    }
+    
     
 }
