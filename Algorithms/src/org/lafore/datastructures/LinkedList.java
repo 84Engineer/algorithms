83dfec4a6d11e016cf9cdfd1a4c3ab63feaa4459
LocalUser <LocalUser@CalcPC>
2016-08-04 21:39:31 +0300

    initial list completion

--- Algorithms/src/org/lafore/datastructures/LinkedList.java
+++ Algorithms/src/org/lafore/datastructures/LinkedList.java
@@ -10,7 +10,7 @@ package org.lafore.datastructures;
  * @author oslysenko
  */
 public class LinkedList<T> {
-    
+
     private Link first;
 
     private class Link {
@@ -23,12 +23,62 @@ public class LinkedList<T> {
             this.dataId = dataId;
             this.data = data;
         }
-        
+
         public void displayLink() {
-            System.out.println("{ " + dataId + ", " + data + " }");
+            System.out.print("{ " + dataId + ", " + data + " }");
         }
     }
-    
-    
+
+    public LinkedList() {
+        first = null;
+    }
+
+    public boolean isEmpty() {
+        return first == null;
+    }
+
+    public void insertFirst(int dataId, T data) {
+        Link newLink = new Link(dataId, data);
+        newLink.next = first;
+        first = newLink;
+    }
+
+    public Link deleteFirst() {
+        Link temp = first;
+        first = first.next;
+        return temp;
+    }
+
+    public void displayList() {
+        System.out.print("List (first-->last): ");
+        Link current = first;
+        while (current != null) {
+            current.displayLink();
+            current = current.next;
+        }
+        System.out.println("");
+    }
+
+    //Testing linked list
+    public static void main(String[] args) {
+
+        LinkedList<String> ll = new LinkedList();
+        ll.insertFirst(0, "first added");
+        ll.insertFirst(1, "second added");
+        ll.insertFirst(2, "third added");
+        ll.insertFirst(3, "4th added");
+        ll.insertFirst(4, "5th added");
+
+        ll.displayList();
+
+        while (!ll.isEmpty()) {
+            System.out.print("Deleted: ");  
+            ll.deleteFirst().displayLink();
+            System.out.println("");
+        }
+        
+        ll.displayList();
+
+    }
 
 }
